6658424d75ea1ffe5e782a3212c1b8a02da72a80
Murphy <murphyspublic@gmail.com>
2012-04-09 10:43:54 -0600

    Added a bit of documentation

--- main.py
+++ main.py
@@ -42,26 +42,38 @@ class MainForm(QtGui.QWidget):
 
 	# Define some methods to react to signals
 	def refreshStylesheet(self):
+		''' This method opens and applies the custom style sheet for UI '''
 		sheetObj = open('./styles/style.qss')
 		self.setStyleSheet(sheetObj.read())
-		print "Realoaded"
+
 	def switchConvTab(self):
+		''' Switches the UI tab to converter. '''
 		self.ui.stackedWidget.setCurrentWidget(self.ui.page_convert)
+
 	def switchHistTab(self):
+		''' Switches the UI tab to history. '''
 		self.ui.stackedWidget.setCurrentWidget(self.ui.page_history)
+
 	def uiToConvertState(self):
+		''' Controls visibility of ui elements to reflect the convert layout '''
 		self.ui.frame_progress.hide()
 		self.ui.frame_convert.show()
+
 	def uiToProgressState(self):
+		''' Controls visibility of ui elements to reflect the progress layout '''
 		self.ui.frame_progress.show()
 		self.ui.frame_convert.hide()
 
 	def getSceneFile(self):
+		''' This opens a dialog box to be able to pick a maya file '''
 		fileName = QtGui.QFileDialog.getOpenFileName(self, \
 			"Select Scene File", "/", "Maya Files (*.ma *.mb)")
 		self.ui.le_scenePath.setText(fileName[0])
 
 	def startNewConversion(self):
+		''' Tell the controller to instantiate a new scene,
+			switch UI to progress layout and begin timer 
+			to update the log. '''
 		path = self.ui.le_scenePath.text()
 		if (path == ""):
 			return
@@ -70,23 +82,13 @@ class MainForm(QtGui.QWidget):
 		self.uiToProgressState()
 
 	def updateLogProgbar(self):
+		''' If the scene is done converting, stop the watch timer.
+			else, get teh contents of the log for that scene and 
+			update the progress bar '''
 		if self.control.getCurrentStatus() == "True":
 			self.logTimer.stop()
 		self.ui.te_activeLog.setText(self.control.getCurrentLog())
 		self.ui.progressBar.setValue(int(self.control.getCurrentProgress()))
-
-	def updateHistList(self):
-		"""
-		self.ui.list_scenes.clear()
-		hist = self.control.getScenesHistory()
-		hist.reverse()
-		for entry in hist:
-			title = "%s, Date: %s, Progress: %s, Status: %s"%(entry['name'],
-				entry['date'], entry['progress'], entry['finished'])
-			QtGui.QListWidgetItem(title, self.ui.list_scenes)
-		if (self.ui.list_scenes.selectedItems() == []):
-			self.ui.list_scenes.setCurrentRow(0)
-		"""
 
 
 if __name__ == "__main__":
